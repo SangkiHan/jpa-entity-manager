c70136b39662aa760c23a7b0d42cf7d86561e988
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-28 00:38:57 +0900

    feat: EntityEntry 객체 생성

---
+++ src/main/java/persistence/EntityEntry.java
@@ -0,0 +1,10 @@
+package persistence;
+
+public class EntityEntry {
+
+    private final EntityStatus entityStatus;
+
+    public EntityEntry(EntityStatus entityStatus) {
+        this.entityStatus = entityStatus;
+    }
+}
