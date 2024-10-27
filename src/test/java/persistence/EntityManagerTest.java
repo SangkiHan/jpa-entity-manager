819c72768acfc2a986530be0861ea11cb1a3bfbb
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-28 00:39:59 +0900

    test: 테스트코드

--- src/test/java/persistence/EntityManagerTest.java
+++ src/test/java/persistence/EntityManagerTest.java
@@ -4,6 +4,7 @@ import builder.ddl.DDLBuilderData;
 import builder.ddl.builder.CreateQueryBuilder;
 import builder.ddl.builder.DropQueryBuilder;
 import builder.ddl.dataType.DB;
+import builder.dml.DMLBuilderData;
 import database.H2DBConnection;
 import entity.Person;
 import jdbc.JdbcTemplate;
@@ -61,8 +62,8 @@ class EntityManagerTest {
     void findTest() {
         Person person = createPerson(1);
         this.entityManager.persist(person);
-        Object object = this.persistenceContext.findEntity(new EntityKey<>(person.getId(), person.getClass()));
-        assertThat(object)
+        DMLBuilderData dmlBuilderData = this.persistenceContext.findEntity(new EntityKey<>(person.getId(), person.getClass()));
+        assertThat(dmlBuilderData.getEntityInstance())
                 .extracting("id", "name", "age", "email")
                 .contains(1L, "test1", 29, "test@test.com");
     }
@@ -86,9 +87,9 @@ class EntityManagerTest {
         person.changeEmail("changed@test.com");
         this.entityManager.merge(person);
 
-        Object persons = this.persistenceContext.findEntity(new EntityKey<>(person.getId(), person.getClass()));
+        DMLBuilderData dmlBuilderData = this.persistenceContext.findEntity(new EntityKey<>(person.getId(), person.getClass()));
 
-        assertThat(persons)
+        assertThat(dmlBuilderData.getEntityInstance())
                 .extracting("id", "name", "age", "email")
                 .contains(1L, "test1", 29, "changed@test.com");
     }
@@ -102,9 +103,9 @@ class EntityManagerTest {
         person.changeEmail("changed@test.com");
         this.entityManager.merge(person);
 
-        Object persons = this.persistenceContext.getDatabaseSnapshot(new EntityKey<>(person.getId(), person.getClass()));
+        DMLBuilderData dmlBuilderData = this.persistenceContext.getDatabaseSnapshot(new EntityKey<>(person.getId(), person.getClass()));
 
-        assertThat(persons)
+        assertThat(dmlBuilderData.getEntityInstance())
                 .extracting("id", "name", "age", "email")
                 .contains(1L, "test1", 29, "changed@test.com");
     }
--- src/test/java/persistence/PersistenceContextImplTest.java
+++ src/test/java/persistence/PersistenceContextImplTest.java
@@ -1,5 +1,6 @@
 package persistence;
 
+import builder.dml.DMLBuilderData;
 import entity.Person;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 - 영속성 컨텍스트에 저장되어있는 Entity 객체를 제거한다.
 - 영속성 컨텍스트에서 스냅샷을 생성한다.
 - 영속성 컨텍스트에서 스냅샷을 가져온다.
+- 영속성 컨텍스트에서 EntityStatus를 저장 후 저장되어있는 EntityEntry 객체를 가져온다.
 */
 class PersistenceContextImplTest {
 
@@ -23,9 +25,11 @@ class PersistenceContextImplTest {
         Person person = createPerson(1);
         EntityKey<Person> EntityKey = new EntityKey<>(1, Person.class);
 
-        persistenceContext.insertEntity(EntityKey, person);
+        DMLBuilderData dmlBuilderData = DMLBuilderData.createDMLBuilderData(person);
 
-        assertThat(persistenceContext.findEntity(new EntityKey<>(1, Person.class)))
+        persistenceContext.insertEntity(EntityKey, dmlBuilderData);
+
+        assertThat(persistenceContext.findEntity(new EntityKey<>(1, Person.class)).getEntityInstance())
                 .extracting("id", "name", "age", "email")
                 .contains(1L, "test1", 29, "test@test.com");
     }
@@ -35,7 +39,8 @@ class PersistenceContextImplTest {
     void removeTest() {
         PersistenceContextImpl persistenceContext = new PersistenceContextImpl();
         Person person = createPerson(1);
-        IntStream.range(1,3).forEach(i -> persistenceContext.insertEntity(new EntityKey<>(i, Person.class), person));
+        DMLBuilderData dmlBuilderData = DMLBuilderData.createDMLBuilderData(person);
+        IntStream.range(1,3).forEach(i -> persistenceContext.insertEntity(new EntityKey<>(i, Person.class), dmlBuilderData));
 
         persistenceContext.deleteEntity(new EntityKey<>(2, Person.class));
 
@@ -47,7 +52,8 @@ class PersistenceContextImplTest {
     void addDatabaseSnapshotTest() {
         PersistenceContextImpl persistenceContext = new PersistenceContextImpl();
         Person person = createPerson(1);
-        IntStream.range(1,3).forEach(i -> persistenceContext.insertDatabaseSnapshot(new EntityKey<>(i, Person.class), person));
+        DMLBuilderData dmlBuilderData = DMLBuilderData.createDMLBuilderData(person);
+        IntStream.range(1,3).forEach(i -> persistenceContext.insertDatabaseSnapshot(new EntityKey<>(i, Person.class), dmlBuilderData));
 
         assertThat(persistenceContext.findEntity(new EntityKey<>(2, Person.class))).isNull();
     }
@@ -57,10 +63,22 @@ class PersistenceContextImplTest {
     void getDatabaseSnapshotTest() {
         PersistenceContextImpl persistenceContext = new PersistenceContextImpl();
         Person person = createPerson(1);
-        persistenceContext.insertDatabaseSnapshot(new EntityKey<>(person.getId(), Person.class), person);
-        assertThat(persistenceContext.getDatabaseSnapshot(new EntityKey<>(person.getId(), Person.class)))
+        DMLBuilderData dmlBuilderData = DMLBuilderData.createDMLBuilderData(person);
+        persistenceContext.insertDatabaseSnapshot(new EntityKey<>(person.getId(), Person.class), dmlBuilderData);
+        assertThat(persistenceContext.getDatabaseSnapshot(new EntityKey<>(person.getId(), Person.class)).getEntityInstance())
                 .extracting("id", "name", "age", "email")
                 .contains(1L, "test1", 29, "test@test.com");
+    }
+
+    @DisplayName("영속성 컨텍스트에서 EntityStatus를 저장 후 저장되어있는 EntityEntry 객체를 가져온다.")
+    @Test
+    void insertEntityEntryMapTest() {
+        PersistenceContextImpl persistenceContext = new PersistenceContextImpl();
+        Person person = createPerson(1);
+
+        persistenceContext.insertEntityEntryMap(new EntityKey<>(person.getId(), Person.class), EntityStatus.MANAGED);
+        assertThat(persistenceContext.getEntityEntryMap(new EntityKey<>(person.getId(), Person.class)))
+                .extracting("entityStatus").isEqualTo(EntityStatus.MANAGED);
     }
 
     private Person createPerson(int i) {
